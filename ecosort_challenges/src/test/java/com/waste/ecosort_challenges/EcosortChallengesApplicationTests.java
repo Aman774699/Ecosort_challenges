8522ff01c05a59117993b92e17224f5cc522a2e3
Aman Vishwakarma <104434057+Aman774699@users.noreply.github.com>
2024-04-22 23:39:08 +0530

    Add files via upload

---
+++ ecosort_challenges/src/test/java/com/waste/ecosort_challenges/EcosortChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waste.ecosort_challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosortChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
